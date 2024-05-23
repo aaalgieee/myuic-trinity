3cf6ab81a05b87e4a381bfc4cf002caaeb776a19
Al Gabriel Orig <jaworski.orig@gmail.com>
2024-05-24 02:54:49 +0800

    ios: initial commit for SwiftUI

---
+++ ios/App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Al Gabriel on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
